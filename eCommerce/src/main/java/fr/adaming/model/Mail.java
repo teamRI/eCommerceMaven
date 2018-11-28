618d84070e8b3dab2fdc860ad39bccb13aa2a6d8
inti0493 <inti0493@192.168.1.147>
2018-11-28 21:38:16 +0100

    Ronan : import des lib mail et pdf + ajout des pages .xhtml

--- eCommerce/src/main/java/fr/adaming/model/Mail.java
+++ eCommerce/src/main/java/fr/adaming/model/Mail.java
@@ -6,7 +6,6 @@ import java.util.Properties;
 import javax.activation.DataHandler;
 import javax.activation.DataSource;
 import javax.activation.FileDataSource;
-import javax.ejb.Stateless;
 import javax.mail.BodyPart;
 import javax.mail.Message;
 import javax.mail.MessagingException;
