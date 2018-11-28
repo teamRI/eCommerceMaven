47bbf0f550350503a95cc4ed82cac9c446b7a711
inti0493 <inti0493@192.168.1.147>
2018-11-28 17:33:09 +0100

    Ronan : Dao et service client + dossier resources dans webapp

--- eCommerce/src/main/java/fr/adaming/dao/ClientDaoImpl.java
+++ eCommerce/src/main/java/fr/adaming/dao/ClientDaoImpl.java
@@ -2,42 +2,75 @@ package fr.adaming.dao;
 
 import java.util.List;
 
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import fr.adaming.model.Client;
 import fr.adaming.model.Commande;
+import fr.adaming.model.Formateur;
 
-
+@Repository
 public class ClientDaoImpl implements IClientDao {
 
-	@Override
-	public Client addClient(Client cl) {
-		// TODO Auto-generated method stub
-		return null;
+	@Autowired
+	private SessionFactory sf;
+
+	public void setSf(SessionFactory sf) {
+		this.sf = sf;
 	}
 
 	@Override
-	public Client upDateClient(Client cl) {
-		// TODO Auto-generated method stub
-		return null;
+	public Client addClient(Client cl) {
+		Session s = sf.getCurrentSession();
+		s.save(cl);
+		return cl;
+	}
+
+	@Override
+	public int upDateClient(Client cl) {
+		Session s = sf.getCurrentSession();
+		String req = "UPDATE Client cl SET cl.nom=:pNom, cl.adresse=:pAdr, cl.email=:pMail, cl.tel=:pTel WHERE cl.id=:pIdc";
+		Query q = s.createQuery(req);
+		q.setParameter("pIdc", cl.getId());
+		q.setParameter("pNom", cl.getNom());
+		q.setParameter("pAdr", cl.getAdresse());
+		q.setParameter("pMail", cl.getEmail());
+		q.setParameter("pTel", cl.getTel());
+		return q.executeUpdate();
+
 	}
 
 	@Override
 	public int deleteClient(Client cl) {
-		// TODO Auto-generated method stub
-		return 0;
+		Session s = sf.getCurrentSession();
+		String req = "DELETE Client cl WHERE cl.id=:pIdc";
+		Query q = s.createQuery(req);
+		q.setParameter("pIdc", cl.getId());
+		return q.executeUpdate();
 	}
 
 	@Override
 	public Client getClient(Client cl) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = sf.getCurrentSession();
+		Client clOut = (Client) s.get(Client.class, cl.getId());
+		return clOut;
 	}
 
 	@Override
 	public Client isExist(Client c) {
-		// TODO Auto-generated method stub
-		return null;
+		Session s = sf.getCurrentSession();
+
+		// HQL
+		String req = "FROM Client as cl WHERE cl.email=:pMail AND cl.nom=:pNom";
+
+		Query query = s.createQuery(req);
+		query.setParameter("pMail", c.getEmail());
+		query.setParameter("pMdp", c.getNom());
+
+		return (Client) query.uniqueResult();
 	}
 
-	
 }
--- eCommerce/src/main/java/fr/adaming/dao/IClientDao.java
+++ eCommerce/src/main/java/fr/adaming/dao/IClientDao.java
@@ -7,7 +7,7 @@ import fr.adaming.model.Client;
 public interface IClientDao {
 
 	public Client addClient(Client cl);
-	public Client upDateClient(Client cl);
+	public int upDateClient(Client cl);
 	public int deleteClient(Client cl);
 	public Client getClient(Client cl);
 	public Client isExist(Client c);
